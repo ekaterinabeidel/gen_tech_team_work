d967dd01abd8c73d7b2a5ce3e64708a6aaa6287f
Shmalii <Shmaliy2@gmai.com>
2024-06-05 14:20:24 +0200

    END-task#1

--- task#1.sql
+++ task#1.sql
@@ -2,9 +2,10 @@ SELECT
   *,
   CASE 
     WHEN
+    CategoryID IN (3, 5)
+    AND
     Price >= 20
     THEN Price * 1.125 
     ELSE Price 
   END AS Price_up
-FROM Products
-WHERE CategoryID IN (3, 5)
+FROM Products;
