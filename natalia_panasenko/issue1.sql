3d9fef56bd5720896a10a9da7d0326f7cfaadd56
NataliaPnk <nataliaa.pnnk@gmail.com>
2024-06-05 14:07:41 +0200

    solved:task1

---
+++ natalia_panasenko/issue1.sql
@@ -0,0 +1,12 @@
+Вывести все товары, причем для товаров 
+из категорий 3 и 5 и ценой от 20 EUR 
+добавить наценку в 12.5%
+проекция: все поля + поле Price_up
+
+SELECT *,
+CASE 
+WHEN Price >= 20 AND (CategoryID = 3 OR CategoryID = 5) THEN Price * 1.125
+ ELSE Price
+ END AS Price_up
+FROM 
+Products
