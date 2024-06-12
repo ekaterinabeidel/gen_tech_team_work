5e586cd4a4c234a39239f0b8ef84c20f7810f2f3
Slava <slavok.v005@mail.ru>
2024-06-12 13:15:25 +0200

    new

--- task40.sql
+++
@@ -1,9 +0,0 @@
---Вывести все товары, причем для товаров из категорий 3 и 5 и ценой от 20 EUR добавить наценку в 12.5%
---проекция: все поля + поле Price_up
-
-SELECT *,
-CASE
-    WHEN Products.CategoryID IN (3, 5) AND Price >= 20 THEN Price * 1.125
-    ELSE Price
-END AS price_up
-FROM Products;
