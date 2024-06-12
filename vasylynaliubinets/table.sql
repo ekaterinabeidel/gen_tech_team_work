47bcaddedbf92c218960705ce70955d9c5674e57
Vasylyna <alina8989_08@ukr.net>
2024-06-12 14:06:08 +0200

    table

--- vasylynaliubinets/table.sql
+++ vasylynaliubinets/table.sql
@@ -1,12 +1,13 @@
 /*Задача. Создать таблицу пожертвований donations */
-create table donations(
-id int primary key auto_increment,
-created_at timestamp default current_timestamp,
 
-amount int not null,
-text varchar(128)
-user_donation_id int not null 
+create table donations (
+    id int primary key auto_increment,
+    created_at timestamp default current_timestamp,
 
-foreign key (user_id) references users(user_id)
-);
+    amount float not null,
+    donator_id int not null,
+    receiver_id int not null,
+
+    foreign key (donator_id) references users (id),
+    foreign key (receiver_id) references users (id)
  
