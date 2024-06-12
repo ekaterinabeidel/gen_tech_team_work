12cfeabbd47cd02c868047854cb6215e21d0d9ec
SergeiStrukov <serega.strukov21@gmail.com>
2024-06-12 16:36:37 +0400

    Task4.sql (correct address)

--- SergeiStrukov/Task3.sql
+++ SergeiStrukov/Task3.sql
@@ -1,6 +1,6 @@
 INSERT INTO donations (amount, currency, description, user1_id, user2_id)
 VALUES
-(1000, 'USD', 'for good things', 2, 2),
+(1000, 'USD', 'for good things', 2, 44),
 (1000, 'RUB', 'for good things', 3, 2),
 (1000000, 'EUR', 'for good things', 4, 2),
 (10000000, 'EUR', 'for good things', 5, 2);
